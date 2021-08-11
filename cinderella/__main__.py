f425a37e0804c4696c3ecfa012447e9392699d35
ETERNAL-BEAST <80637560+ETERNAL-BEAST@users.noreply.github.com>
2021-08-11 23:19:31 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -29,7 +29,7 @@ from cinderella.modules.connection import connect_button
 
 PM_START_TEXT = """
 _Hello_ *{}*
-_My name is_ *{}*\n_A ɪᴍ senorita a unique and quality group managing bot go to the buttons below for more info.,feel free to add to your groups!!_
+_My name is_ *{}*\n_A ɪᴍ Siesta a unique and quality group managing bot go to the buttons below for more info.,feel free to add to your groups!!_
 _Maintained by_ [{}](tg://user?id={})
 """
 
@@ -174,12 +174,12 @@ def send_start(bot, update):
     text = PM_START_TEXT
 
     keyboard = [[InlineKeyboardButton(text="◼️HELPS AND COMMANDS❔",callback_data="help_back"),
-                 InlineKeyboardButton(text="MY OWNER⚡️",url="https://t.me/mbbs_lover")]]
-    keyboard += [[InlineKeyboardButton(text="🔱✪𝗠𝗟𝗢 𝗘𝗠𝗣𝗜𝗥𝗘✪",url="https://t.me/MLO_EMPIRE"),
-                 InlineKeyboardButton(text="🔴SPAMREPORTER🔴",url="https://t.me/spamreporterr")]]
-    keyboard += [[InlineKeyboardButton(text="♦️SUPPORT CHAT💬",url="https://t.me/SENORITA_UPDATES")]]
-    keyboard += [[InlineKeyboardButton(text="🔲 CONNCET 𝐒𝐄𝐍𝐎𝐑𝐈𝐓𝐀™🇮🇳 TO YOUR GROUP🔲", url="http://t.me/MissSenorita_bot?startgroup=true"),
-                 InlineKeyboardButton(text="🔥◾️NETWORK◾️🔥",url="https://t.me/MLO_NETWORK")]]
+                 InlineKeyboardButton(text="MY OWNER⚡️",url="https://t.me/demon_emperor")]]
+    keyboard += [[InlineKeyboardButton(text="ORIGIN GAMING ZONE",url="https://t.me/origingaming"),
+                 InlineKeyboardButton(text="ORIGIN BATTLEGROUND",url="https://t.me/origin_battleground")]]
+    keyboard += [[InlineKeyboardButton(text="CHAT GROUP",url="https://t.me/hexamain")]]
+    keyboard += [[InlineKeyboardButton(text="🔲 CONNECT Siesta TO YOUR GROUP🔲", url="http://t.me/MissSiesta_bot?startgroup=true"),
+                 InlineKeyboardButton(text="🔥◾️NETWORK◾️🔥",url="https://t.me/Origin_Battleground_Broadcast")]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
